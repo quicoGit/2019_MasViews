3eacdcfafcaedc367bea492ff9077d2965d06f4d
Quico Orozco <esi@plumbaria.com>
2018-10-25 18:46:42 +0200

    Commit inicial Ejercicio 2.10. Un botón con gráficos personalizados

---
+++ app/src/main/java/com/plumbaria/e_2_10_masviews/MasViewsActivity.kt
@@ -0,0 +1,12 @@
+package com.plumbaria.e_2_10_masviews
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MasViewsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_mas_views)
+    }
+}

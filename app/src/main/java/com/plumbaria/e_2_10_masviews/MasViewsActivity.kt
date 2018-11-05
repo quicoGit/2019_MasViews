a5c783140daf699dd24af67ed0b62a8ff103a5d7
Quico Orozco <esi@plumbaria.com>
2018-11-05 19:38:53 +0100

    Commit final Ejercicio 2.10 Mas Views

--- app/src/main/java/com/plumbaria/e_2_10_masviews/MasViewsActivity.kt
+++ app/src/main/java/com/plumbaria/e_2_10_masviews/MasViewsActivity.kt
@@ -2,11 +2,17 @@ package com.plumbaria.e_2_10_masviews
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 
-class MasViewsActivity : AppCompatActivity() {
+class MasViewActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_mas_views)
+    }
+
+    fun sePulsa(view:View){
+        Toast.makeText(this,"Pulsado",Toast.LENGTH_SHORT).show()
     }
 }
